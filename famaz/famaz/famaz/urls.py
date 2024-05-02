38a40a98ca1572364790bab64ed86f55823756a0
Bakijazzman <obafemiolabayo@gmail.com>
2024-05-02 07:29:52 +0100

    started the core app for the project

--- famaz/famaz/famaz/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin # type: ignore
-from django.urls import path # type: ignore
-
-from core.views import index
-urlpatterns = [
-    path('', index, name='index'),
-    path('admin/', admin.site.urls),
-]
