3c0c51fba8ea9f449cde905747a0eb1d91a3c53b
Bakijazzman <obafemiolabayo@gmail.com>
2024-05-09 10:22:12 +0100

    remove from cart logic

--- famaz/core/urls.py
+++ famaz/core/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path # type: ignore
 from .views import (
     ItemDetailView, 
-    checkout, 
-    HomeView, 
-    item_list,
+    HomeView,
+    remove_from_cart, 
     add_to_cart)
 
 app_name = 'core'
@@ -11,7 +10,6 @@ app_name = 'core'
 urlpatterns = [
     path('', HomeView.as_view(), name='index'),
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
-    path('checkout/', checkout, name='checkout'),
-    path('item_list/', item_list, name='item_list'),
-    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart')
+    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
+    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart')
 ]
