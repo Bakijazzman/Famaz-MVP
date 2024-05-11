6fa6f065bc363f4c15880c919aca14791dcb3ec2
Bakijazzman <obafemiolabayo@gmail.com>
2024-05-11 08:51:40 +0100

    touch up

--- famaz/core/urls.py
+++ famaz/core/urls.py
@@ -3,7 +3,8 @@ from .views import (
     ItemDetailView, 
     HomeView,
     remove_from_cart, 
-    add_to_cart)
+    add_to_cart,
+    checkout)
 
 app_name = 'core'
 
@@ -11,5 +12,6 @@ urlpatterns = [
     path('', HomeView.as_view(), name='index'),
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
     path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
-    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart')
+    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
+    path("checkout/", checkout, name="checkout")
 ]
