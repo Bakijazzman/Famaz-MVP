29d719bd1b1ad4499d2ea6f43f04c4b4bc07cd51
Bakijazzman <obafemiolabayo@gmail.com>
2024-05-15 09:04:26 +0100

    incorporated the login and logout functionalities

--- famaz2/famaz/core/models.py
+++ famaz2/famaz/core/models.py
@@ -33,6 +33,9 @@ class Product(models.Model):
     Category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     description = models.TextField()
     image = models.ImageField(upload_to="uploads/product/")
+    #add the sales look
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=50)
     
     def __str__(self):
         return self.name
--- famaz2/famaz/core/urls.py
+++ famaz2/famaz/core/urls.py
@@ -1,7 +1,10 @@
-from .views import index
+from .views import index, about, login_user, logout_user
 from django.urls import path
 
 
 urlpatterns = [
     path('', index, name="index"),
+    path('about/', about, name="about"),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
