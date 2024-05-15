00c0ba861052aed8cd2ab1708ad641acf96241a9
Bakijazzman <obafemiolabayo@gmail.com>
2024-05-15 09:37:39 +0100

    removed old files

--- famaz/core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin # type: ignore
-
-# Register your models here.
-from .models import Item, Order, OrderItem
-
-admin.site.register(Order)
-admin.site.register(OrderItem)
-admin.site.register(Item)
