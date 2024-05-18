c527f9e459ada2940a0da56eabfeaf4cedccd8f5
Bakijazzman <obafemiolabayo@gmail.com>
2024-05-18 21:26:10 +0100

    adeed to cart functionality fixed

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from core.models import Product
+from django.http import JsonResponse
 
 
 def cart_summary(request):
@@ -6,7 +9,15 @@ def cart_summary(request):
 
 
 def cart_add(request):
-    pass
+    #Gett cart
+    cart = Cart(request)
+    #test for post
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        product = get_object_or_404(Product, id=product_id)
+        cart.add(product=product)
+        response = JsonResponse({'Product Name: ': product.name})
+        return response
 
 
 def cart_delete(delete):
