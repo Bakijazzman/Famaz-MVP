db6ec71638214765af75f9cc0effdcee7dd84304
Bakijazzman <obafemiolabayo@gmail.com>
2024-05-18 22:06:37 +0100

    updates the cart count functionality

--- cart/views.py
+++ cart/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .cart import Cart
 from core.models import Product
 from django.http import JsonResponse
-
+from django.contrib import messages
 
 def cart_summary(request):
     return render(request, 'cart_summary.html', {})
@@ -16,7 +16,11 @@ def cart_add(request):
         product_id = int(request.POST.get('product_id'))
         product = get_object_or_404(Product, id=product_id)
         cart.add(product=product)
-        response = JsonResponse({'Product Name: ': product.name})
+        #get cart qunatity
+        cart_quantity = cart.__len__()
+        # response = JsonResponse({'Product Name: ': product.name})
+        response = JsonResponse({'qty: ': cart_quantity})
+        messages.success(request, ("Product Added To Cart..."))
         return response
 
 
