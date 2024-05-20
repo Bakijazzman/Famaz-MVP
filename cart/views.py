54313b88ad35855bab54195f48dd470159a94894
Bakijazzman <obafemiolabayo@gmail.com>
2024-05-20 07:11:41 +0100

    finished the delete functionality

--- cart/views.py
+++ cart/views.py
@@ -5,7 +5,10 @@ from django.http import JsonResponse
 from django.contrib import messages
 
 def cart_summary(request):
-    return render(request, 'cart_summary.html', {})
+    cart = Cart(request)
+    cart_products = cart.get_prod
+    quantities = cart.get_quants
+    return render(request, 'cart_summary.html', {"cart_products":cart_products, "quantities":quantities})
 
 
 def cart_add(request):
@@ -14,19 +17,33 @@ def cart_add(request):
     #test for post
     if request.POST.get('action') == 'post':
         product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
         product = get_object_or_404(Product, id=product_id)
-        cart.add(product=product)
+        cart.add(product=product, quantity=product_qty)
         #get cart qunatity
         cart_quantity = cart.__len__()
-        # response = JsonResponse({'Product Name: ': product.name})
         response = JsonResponse({'qty: ': cart_quantity})
         messages.success(request, ("Product Added To Cart..."))
         return response
 
 
-def cart_delete(delete):
-    pass
+def cart_delete(request):
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        cart.delete(product=product_id)
+        response =JsonResponse({'product': product_id})
+        messages.success(request, ("Item removed, you can re-add at the product page"))
+        return response
     
     
 def cart_update(request):
-    pass
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
+        cart.update(product=product_id, quantity=product_qty)
+        response =JsonResponse({'qty': product_qty})
+        messages.success(request, ("Your Cart Has Been Updated... reload page to see changes "))
+        return response
+        #return redirect('cart_summary')
