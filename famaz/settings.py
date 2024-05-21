124ba204c04b31c31ecb124759f2d5bc087ce8a4
Bakijazzman <obafemiolabayo@gmail.com>
2024-05-21 18:50:11 +0100

    deploy4

--- famaz/settings.py
+++ famaz/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = ["https://famaz-mvp.onrender.com", ""]
+ALLOWED_HOSTS = ["famaz-mvp.onrender.com", ""]
 # ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 # allowed_host = os.environ.get("ALLOWED_HOSTS", "")
 # ALLOWED_HOST = allowed_host.split(" ")
